d6836746424896128c0fd4f858ae5e46375a361d
xKrat19 <xkrat19@gmail.com>
2026-03-08 13:34:24 +0100

    Display of the maze (only walls)

--- mazegen/visual.py
+++ mazegen/visual.py
@@ -1,22 +1,17 @@
 import time
-from enum import Enum
-from typing import List, Any
 # time.sleep(0.5)
-import sys
+from enum import Enum
+from .generator import MazeGenerator
 
 # \033[H -> Mueve el cursor de la terminal a 0,0(arriba de todo)
 # \033[J -> Limpia todo lo que este por debajo del cursor
 # \033[3J -> Borra el historial de desplazamiento
 # \033[?25l -> Oculta el cursor en la terminal
 
-# +---+---+
-# | ■     |
-# +---+---+
-
-class DRAW(Enum):
+class Draw(Enum):
     # +---+---+
     # | ■     |
-    # +---+---+
+    # +   +---+
     WALL_H= "---" # Wall for North & South
     EMPTY_H= "   " # No-Wall for North & South
     WALL_V= "|" # Wall for East & West
@@ -25,8 +20,46 @@ class DRAW(Enum):
     POINT="■"
 
 
-def render_maze(grid: List[List[Any]]):
-
+def render_maze(maze: MazeGenerator) -> str:
+# def render_maze(grid: list[list]) -> str:
+    # +---+---+ (Line Norte de 0,0)
+    # | ■     | (Line Center de 0,0)
+    # +---+---+ (Line Norte de 1,0)
+    grid = maze.grid
+    # clean_terminal()
+    display = ""
+    for y in range(maze.height):
+        line_north = ""
+        line_center = ""
+        for x in range(maze.width):
+            line_north += Draw.CROSS.value # Pinta las esquinas
+            if grid[y][x] & 1: # Comprueba si esta abierto hacia el norte
+                line_north += Draw.WALL_H.value
+            else:
+                line_north += Draw.EMPTY_H.value
+            if grid[y][x] & 8: # Comprueba si esta abieto hacia el oeste
+                line_center += Draw.WALL_V.value
+            else:
+                line_center += Draw.EMPTY_V.value
+            line_center += Draw.EMPTY_H.value
+        # Cierre del borde derecha
+        line_north += Draw.CROSS.value # Añade el ultimo cross
+        if grid[y][maze.width - 1] & 2:
+            line_center += Draw.WALL_V.value
+        else:
+            line_center += Draw.EMPTY_V.value
+        # Junta todas las lines para pintar la celda
+        display += line_north + "\n" + line_center + "\n" 
+    line_final = ""
+    for x in range(maze.width):
+        line_final += Draw.CROSS.value
+        if grid[y - 1][x] & 4: # Check si tiene pared al sur
+            line_final += Draw.WALL_H.value
+        else:
+            line_final += Draw.EMPTY_H.value
+    line_final += Draw.CROSS.value
+    display += line_final
+    return display
 
 def disable_cursor():
     print("\033[?25l")
@@ -36,6 +69,6 @@ def enable_cursor():
     print("\033[?25h")
 
 
-def clean_terminal():
-    print("\033[2J\033[3J\033[H\033")
+def clean_terminal() -> str:
+    print("\033[2J\033[3J\033[H\033\033[?25l")
 
