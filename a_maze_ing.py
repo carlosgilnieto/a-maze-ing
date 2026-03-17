b865f5c632ef991cc1033e0830785912140c9fa8
carlosgilnieto <xkrat19@gmail.com>
2026-03-17 11:00:16 +0100

    fix: refactor main loop in a_maze_ing.py for improved readability and structure

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -48,59 +48,63 @@ def main() -> None:
     show_path = False
     anim_path = maze.animation
     generate = True
-    while True:
-        if maze.animation:
-            if generate:
-                mazegen.render_generation(maze)
-                mazegen.render_solve(maze)
-        else:
-            if generate:
-                maze.generate()
-                maze.calculate_path()
+    try:
+        while True:
+            if maze.animation:
+                if generate:
+                    mazegen.render_generation(maze)
+                    mazegen.render_solve(maze)
 
-        if show_path:
-            mazegen.render_path(maze, maze.animation and anim_path,
-                                pallet[color_idx])
-        mazegen.render_maze(maze, terminal=True, show_path=show_path,
-                            color=pallet[color_idx])
+            else:
+                if generate:
+                    maze.generate()
+                    maze.calculate_path()
 
-        mazegen.generate_output(maze)
-        print("=== A-Maze-ing ===")
-        print("[1]. Re-generate a new maze\n"
-              f"[2]. {'Hide' if show_path else 'Show'} path from "
-              "entry to exit\n"
-              "[3]. Change maze colors\n"
-              "[4]. Quit\n")
+            if show_path:
+                mazegen.render_path(maze, maze.animation and anim_path,
+                                    pallet[color_idx])
+            mazegen.render_maze(maze, terminal=True, show_path=show_path,
+                                color=pallet[color_idx])
 
-        option = input("\033[?25h" + msg + "Choise? (1-4):")
-        options = ["1", "2", "3", "4"]
+            mazegen.generate_output(maze)
+            print("=== A-Maze-ing ===")
+            print("[1]. Re-generate a new maze\n"
+                  f"[2]. {'Hide' if show_path else 'Show'} path from "
+                  "entry to exit\n"
+                  "[3]. Change maze colors\n"
+                  "[4]. Quit\n")
+            option = input("\033[?25h" + msg + "Choise? (1-4):")
+            options = ["1", "2", "3", "4"]
 
-        try:
-            index = int(option) - 1
-            if index < 0:
-                raise ValueError
+            try:
+                index = int(option) - 1
+                if index < 0:
+                    raise ValueError
 
-            option = options[index]
-            msg = ""
-            generate = False
-            anim_path = False
+                option = options[index]
+                msg = ""
+                generate = False
+                anim_path = False
 
-            if option == "1":
-                generate = True
-                anim_path = True
-                pass
-            elif option == "2":
-                show_path = not show_path
-                anim_path = True
-            elif option == "3":
-                color_idx = (color_idx + 1) % len(pallet)
-            elif option == "4":
-                sys.stdout.write("\033[2J\033[3J\033[H\033[?25h")
-                sys.exit()
-        except (IndexError, ValueError):
-            msg = "\033[31mSelect a valid option (1-4).\033[0m\n"
-            generate = False
-            anim_path = False
+                if option == "1":
+                    generate = True
+                    anim_path = True
+                    pass
+                elif option == "2":
+                    show_path = not show_path
+                    anim_path = True
+                elif option == "3":
+                    color_idx = (color_idx + 1) % len(pallet)
+                elif option == "4":
+                    sys.stdout.write("\033[2J\033[3J\033[H\033[?25h")
+                    sys.exit()
+            except (IndexError, ValueError):
+                msg = "\033[31mSelect a valid option (1-4).\033[0m\n"
+                generate = False
+                anim_path = False
+    except KeyboardInterrupt:
+        print("\033[?25h\033[2J\033[3J\033[H")
+        sys.exit("\033[91mProcess interrupted\033[0m")
 
 
 if __name__ == "__main__":
